9b85d1345d8f6a405ae39d7226da0556d85dade6
Alesia170 <alesya.17039722@yandex.ru>
2025-10-04 09:05:58 +0300

    Update ManagerSaveException.java

--- src/ru/practicum/exception/ManagerSaveException.java
+++ src/ru/practicum/exception/ManagerSaveException.java
@@ -1,6 +1,6 @@
 package ru.practicum.exception;
 
-public class ManagerSaveException extends RuntimeException{
+public class ManagerSaveException extends RuntimeException {
 
     public ManagerSaveException(String message, Throwable cause) {
         super(message, cause);
